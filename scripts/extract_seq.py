59e29636ad2605190e4722907cbef0169de804a0
zhaoyang-li-bio <1045700857@qq.com>
2024-08-13 07:44:01 +0800

    scripts/

--- scripts/extract_seq.py
+++ scripts/extract_seq.py
@@ -11,17 +11,37 @@ def extract_seq(seq_file ,extract_name):
 
     return seq_dict
 
+class FastalKit:
+    """
+    用于提取基因序列。
+    """
+    def __init__(self,seq_f,option) -> None:
+        self.records = SeqIO.parse(seq_f,'fasta')
+    
+    def extract_by_option(self,options):
+        """从一个fasta文件中,按列表进行分类，给出字典。
+        """
+        for record in self.records:
+            for option in options:
+                if option in record.description:
+                    
+         
+
+        
+
 def main():
     #提取想要的序列
-    file = 'split_res.fasta'
-    extract_name = ["s875","s4816","s2451","s213","s1960","s4346","s140"]
-    extract_l = extract_seq(seq_file=file,extract_name=extract_name)
+    file = './data/spaovirus.fasta'
+    # extract_name = ["s875","s4816","s2451","s213","s1960","s4346","s140"]
+    # extract_l = extract_seq(seq_file=file,extract_name=extract_name)
 
 #   输出新的文件
-    f = open("提取的序列.fasta",'wt')
-    for k,v in extract_l.items():
-        f.write(f">{k}\n{v}\n")
-    f.close()
+    # f = open("提取的序列.fasta",'wt')
+    # for k,v in extract_l.items():
+    #     f.write(f">{k}\n{v}\n")
+    # f.close()
+    
+    FastalKit(seq_f=file)
 
 if __name__ == "__main__":
     main()
